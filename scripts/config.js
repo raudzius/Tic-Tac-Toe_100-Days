3df5b4e23d8b8a76e6d4507985daf7d674b8d916
Mykolas Raudžius <raudziusmykolas@gmail.com>
2022-06-10 00:03:12 +0300

    feat: validating player name input

--- scripts/config.js
+++ scripts/config.js
@@ -4,6 +4,20 @@ function openPlayerConfig() {
 }
 
 function closePlayerConfig() {
-  backdropElement.style.display = 'none';
   playerConfigOverlayElement.style.display = 'none';
+  backdropElement.style.display = 'none';
+  formElement.firstElementChild.classList.remove('error');
+  errorsOutputElement.textContent = '';
+}
+
+function savePlayerConfig(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const enteredPlayerName = formData.get('playername').trim();
+
+  if (!enteredPlayerName) {
+    event.target.firstElementChild.classList.add('error');
+    errorsOutputElement.textContent = 'Please enter a valid name!';
+    return;
+  }
 }
