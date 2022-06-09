82d5ac66e09a0fc51b88480d409e2ec3dae545af
Mykolas Raudžius <raudziusmykolas@gmail.com>
2022-06-09 23:14:08 +0300

    feat: showing and hiding configuration modal

---
+++ scripts/config.js
@@ -0,0 +1,9 @@
+function openPlayerConfig() {
+  backdropElement.style.display = 'block';
+  playerConfigOverlayElement.style.display = 'block';
+}
+
+function closePlayerConfig() {
+  backdropElement.style.display = 'none';
+  playerConfigOverlayElement.style.display = 'none';
+}
