55849b5ee054c668e17b8e69259c556213271726
Mykolas Raudžius <raudziusmykolas@gmail.com>
2022-06-10 19:46:10 +0300

    feat: game ending & restart logic

--- scripts/game.js
+++ scripts/game.js
@@ -1,8 +1,30 @@
+function resetGameStatus() {
+  activePlayer = 0;
+  currentRound = 1;
+  gameIsOver = false;
+  gameOverElement.firstElementChild.innerHTML = '<h2>You won, <span id="winner-name">PLAYER NAME</span></h2>';
+  gameOverElement.style.display = 'none';
+
+  let gameboardIndex = 0;
+
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      gameData[i][j] = 0;
+      const gameBoardItemElement = gameBoardElement.children[gameboardIndex];
+      gameBoardItemElement.textContent = '';
+      gameBoardItemElement.classList.remove('disabled');
+      gameboardIndex++;
+    }
+  }
+}
+
 function startNewGame() {
   if (!players[0].name || !players[1].name) {
     alert('Please set custom player names for both players!');
     return;
   }
+
+  resetGameStatus();
 
   activePlayerNameElement.textContent = players[activePlayer].name;
   gameAreaElement.style.display = 'block';
@@ -14,7 +36,7 @@ function switchPlayer() {
 }
 
 function selectGameField(event) {
-  if (event.target.tagName !== 'LI') return;
+  if (event.target.tagName !== 'LI' || gameIsOver) return;
 
   const selectedField = event.target;
   const selectedColumn = selectedField.dataset.col - 1;
@@ -31,7 +53,8 @@ function selectGameField(event) {
   gameData[selectedRow][selectedColumn] = activePlayer + 1;
 
   const winnerId = checkForGameOver();
-  console.log(winnerId);
+
+  if (winnerId !== 0) endGame(winnerId);
 
   currentRound++;
   switchPlayer();
@@ -66,4 +89,16 @@ function checkForGameOver() {
   }
 
   return 0;
+}
+
+function endGame(winnerId) {
+  gameIsOver = true;
+  gameOverElement.style.display = 'block';
+
+  if (winnerId > 0) {
+    const winnerName = players[winnerId - 1].name;
+    gameOverElement.firstElementChild.firstElementChild.textContent = winnerName;
+  } else {
+    gameOverElement.firstElementChild.textContent = "It's a draw!";
+  }
 }
